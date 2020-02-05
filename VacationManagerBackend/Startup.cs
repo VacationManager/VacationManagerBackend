7cd26b6128b7b8211c982b1d3c7e6d934e73382d
Leon <leon.bruewer@tobit.software>
2020-02-05 13:37:33 +0100

    Add httpcontextaccessor to startup

--- VacationManagerBackend/Startup.cs
+++ VacationManagerBackend/Startup.cs
@@ -28,6 +28,8 @@ namespace VacationManagerBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+
             services.Configure<DbConfig>(Configuration.GetSection("DbConfig"));
 
             services.AddSingleton<IDbHelper, DbHelper>();
