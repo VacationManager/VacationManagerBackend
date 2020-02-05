5bf7a6bfb2fb69188987209abfed081e93bea165
Niklas Knoll <Niklas.Knoll.64@gmail.com>
2020-02-05 10:14:21 +0100

    Create AccessTokenPayload model

---
+++ VacationManagerBackend/Models/AccessTokenPayload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VacationManagerBackend.Models
+{
+    public class AccessTokenPayload
+    {
+        public int UserId { get; set; }
+        public int DepartmentId { get; set; }
+        public string LastName { get; set; }
+        public string Firstname { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public bool IsManager { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
