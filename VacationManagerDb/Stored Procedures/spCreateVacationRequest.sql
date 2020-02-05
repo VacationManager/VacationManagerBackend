4324ba5d28db9ca64391fd99d57b63781b05f4ec
Leon <leon.bruewer@tobit.software>
2020-02-05 13:24:12 +0100

    Add controller

--- VacationManagerDb/Stored Procedures/spCreateVacationRequest.sql
+++ VacationManagerDb/Stored Procedures/spCreateVacationRequest.sql
@@ -1,14 +1,19 @@
-﻿CREATE PROCEDURE [dbo].[spCreateVacationRequest]
+﻿CREATE TYPE [HolidayList] AS TABLE(
+	[Day] DATE
+)
+GO
+CREATE PROCEDURE [dbo].[spCreateVacationRequest]
 	@userId int,
 	@startTime datetime2,
 	@endTime datetime2,
-	@annotation nvarchar(max)
+	@annotation nvarchar(max),
+	@holidays [HolidayList] READONLY
 AS
 	IF (@endTime <= @startTime)
 	BEGIN
 		RAISERROR(N'StartTime must be smaller than EndTime', 10, 1)
 	END
-	IF (EXISTS(SELECT TOP 1 1 FROM [viVacationRequest] WHERE [UserId] = @userId AND [StartTime] >= @startTime AND [EndTime] < @endTime))
+	IF (EXISTS(SELECT TOP 1 1 FROM [viVacationRequest] WHERE [UserId] = @userId AND [StartTime] >= @startTime AND [EndTime] < @endTime AND [RequestState] != 2))
 	BEGIN
 		RETURN -1
 	END
@@ -21,7 +26,7 @@ AS
 	DECLARE @currentTime datetime2 = @startTime
 	WHILE (@currentTime <= @endTime)
 	BEGIN
-		IF (EXISTS(SELECT TOP 1 1 FROM [viBusinessDay] WHERE [Weekday] = DATEPART(DW, @currentTime) - 1 AND [IsInUse] = 1))
+		IF (EXISTS(SELECT TOP 1 1 FROM [viBusinessDay] WHERE [Weekday] = DATEPART(DW, @currentTime) - 1 AND [IsInUse] = 1) AND NOT EXISTS(SELECT TOP 1 1 FROM @holidays WHERE [Day] = CAST(@currentTime AS date)))
 		BEGIN
 			INSERT INTO @vacationSlot (
 				[Date]
