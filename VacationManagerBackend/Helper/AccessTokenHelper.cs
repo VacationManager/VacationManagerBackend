1b177c80cc3b91df5ae07cd2a92b5267b4ddb1d2
Niklas Knoll <Niklas.Knoll.64@gmail.com>
2020-02-05 11:39:42 +0100

    Create IsTokenValid

--- VacationManagerBackend/Helper/AccessTokenHelper.cs
+++ VacationManagerBackend/Helper/AccessTokenHelper.cs
@@ -6,13 +6,37 @@ namespace VacationManagerBackend.Helper
 {
     public class AccessTokenHelper : IAccessTokenHelper
     {
+        const string secret = "DasISTeinMegaSICHERERSecretStRING";
+
         public string GenerateAccessToken(AccessTokenPayload tokenPayload)
         {
-            var sign = HashSignatureProvider.CreateHS256("DasISTeinMegaSICHERERSecretStRING");
+            var sign = HashSignatureProvider.CreateHS256(secret);
             var jwt = new JsonWebToken<AccessTokenPayload>(tokenPayload, sign);
             var str = jwt.ToEncodedString();
 
             return str;
+        }
+
+        public bool IsTokenValid(string accessToken)
+        {
+            if (!string.IsNullOrEmpty(accessToken) && !string.IsNullOrWhiteSpace(accessToken))
+            {
+                try
+                {
+                    var parser = new JsonWebToken<AccessTokenPayload>.Parser(accessToken);
+                    var sign = HashSignatureProvider.CreateHS256(secret);
+                    var isValid = parser.Verify(sign);
+
+                    return isValid;
+                }
+
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
