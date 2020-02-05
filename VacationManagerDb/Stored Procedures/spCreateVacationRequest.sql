0d28aa90bcc919932b50d4979d34ce90872f83a4
Leon <leon.bruewer@tobit.software>
2020-02-05 13:05:30 +0100

    Edit stored procedure

--- VacationManagerDb/Stored Procedures/spCreateVacationRequest.sql
+++ VacationManagerDb/Stored Procedures/spCreateVacationRequest.sql
@@ -12,28 +12,73 @@ AS
 	BEGIN
 		RETURN -1
 	END
-	DECLARE @duration DECIMAL(10,1) = 0
+
+	DECLARE @vacationSlot TABLE (
+		[Date] DATE
+		,[DayTimeType] BIT
+	)
+
 	DECLARE @currentTime datetime2 = @startTime
-	WHILE (@startTime <= @endTime)
+	WHILE (@currentTime <= @endTime)
 	BEGIN
 		IF (EXISTS(SELECT TOP 1 1 FROM [viBusinessDay] WHERE [Weekday] = DATEPART(DW, @currentTime) - 1 AND [IsInUse] = 1))
 		BEGIN
-			SET @duration = @duration + 0.5
+			INSERT INTO @vacationSlot (
+				[Date]
+				,[DayTimeType]
+			) VALUES (
+				@currentTime
+				,CASE WHEN CAST(@currentTime AS TIME) < '12:00' THEN 0 ELSE 1 END
+			)
 		END
-		SET @currentTime = DATEADD(HOUR, 0.5, @currentTime)
+		SET @currentTime = DATEADD(DAY, 0.5, @currentTime)
+	END
+
+	DECLARE @startYearSlotCount int =  (SELECT DISTINCT COUNT([S].[Id])
+															FROM [viVacationSlot] [S]
+															LEFT JOIN [viVacationRequest] [R]
+															ON [S].[VacationRequestId] = [R].[Id]
+															WHERE [UserId] = @userId
+															AND [RequestState] != 2
+															GROUP BY [Date]
+															HAVING YEAR([Date]) = YEAR(@startTime)) + (SELECT COUNT(1) FROM @vacationSlot GROUP BY [Date] HAVING YEAR([Date]) = YEAR(@startTime))
+
+	DECLARE @endYearSlotCount int =  (SELECT DISTINCT COUNT([S].[Id])
+													FROM [viVacationSlot] [S]
+													LEFT JOIN [viVacationRequest] [R]
+													ON [S].[VacationRequestId] = [R].[Id]
+													WHERE [UserId] = @userId
+													AND [RequestState] != 2
+													GROUP BY [Date]
+													HAVING YEAR([Date]) = YEAR(@endTime)) + (SELECT COUNT(1) FROM @vacationSlot GROUP BY [Date] HAVING YEAR([Date]) = YEAR(@endTime))
+
+	DECLARE @slotsPerYear int = (SELECT [VacationDayCount] FROM [viUser] WHERE [Id] = @userId) * 2
+
+	IF (@startYearSlotCount > @slotsPerYear)
+	BEGIN
+		RETURN 0
 	END
 
 	INSERT INTO [VacationRequest] (
 		[UserId]
 		,[StartTime]
 		,[EndTime]
-		,[Duration]
 		,[Annotation]
 	) VALUES (
 		@userId
 		,@startTime
 		,@endTime
-		,@duration
 		,@annotation
 	)
-RETURN 0
+	DECLARE @requestId int = SCOPE_IDENTITY()
+
+	INSERT INTO [VacationSlot] (
+		[VacationRequestId]
+		,[Date]
+		,[DayTimeType]
+	)
+	SELECT  @requestId
+			,[Date]
+			,[DayTimeType]
+		FROM @vacationSlot
+RETURN 1
