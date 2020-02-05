ac20c0eff416f65dbdf790b15ab4743650472ccb
Leon <leon.bruewer@tobit.software>
2020-02-05 08:54:01 +0100

    Set up db connection

---
+++ VacationManagerBackend/Models/Config/DbConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace VacationManagerBackend.Models.Config
+{
+    public class DbConfig
+    {
+        public string ConnectionString { get; set; }
+    }
+}
--- VacationManagerBackend/Startup.cs
+++ VacationManagerBackend/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using VacationManagerBackend.Models.Config;
 
 namespace VacationManagerBackend
 {
@@ -18,6 +19,8 @@ namespace VacationManagerBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DbConfig>(Configuration.GetSection("DbConfig"));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
