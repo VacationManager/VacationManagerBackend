bffa031a29f05cfbd4da5ee4a83e4d50516b3b5a
Niklas Knoll <Niklas.Knoll.64@gmail.com>
2020-02-05 09:48:46 +0100

    Prepare UserRepository

---
+++ VacationManagerBackend/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using VacationManagerBackend.Interfaces.Helper;
+
+namespace VacationManagerBackend.Repositories
+{
+    public class UserRepository
+    {
+        IDbHelper _dbHelper;
+
+        public UserRepository(IDbHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+    }
+}
