ea683d5d29b0a80e1e2e5e037942b0be90b4d38e
Leon <leon.bruewer@tobit.software>
2020-02-05 13:30:07 +0100

    Add endpoint to create vacation request

--- VacationManagerBackend/Controllers/VacationController.cs
+++ VacationManagerBackend/Controllers/VacationController.cs
@@ -3,14 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VacationManagerBackend.Interfaces.Helper;
+using VacationManagerBackend.Interfaces.Repositories;
+using VacationManagerBackend.Models;
 
 namespace VacationManagerBackend.Controllers
 {
     public class VacationController : Controller
     {
-        public VacationController()
-        {
+        private readonly IAccessTokenProvider _accessTokenProvider;
+        private readonly IVacationRepository _vacationRepository;
 
+        public VacationController(IAccessTokenProvider accessTokenProvider, IVacationRepository vacationRepository)
+        {
+            _accessTokenProvider = accessTokenProvider;
+            _vacationRepository = vacationRepository;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] VacationRequest request)
+        {
+            if (request == null)
+            {
+                // TODO: log error
+                return BadRequest();
+            }
+            var user = _accessTokenProvider.GetTokenPayload();
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            request.UserId = user.UserId;
+            _vacationRepository.CreateVacationRequest(request);
+            return Ok();
         }
     }
 }
