171312ff12726eeb32e931ecee0c061c35d47697
Niklas Knoll <Niklas.Knoll.64@gmail.com>
2020-02-06 10:53:54 +0100

    Create spSetUser

---
+++ VacationManagerDb/Stored Procedures/spSetUser.sql
@@ -0,0 +1,36 @@
+﻿CREATE PROCEDURE [dbo].[spSetUser]
+	@Id INT = NULL,
+	@DepartmentId INT = NULL,
+	@FirstName NVARCHAR(256) = NULL,
+	@LastName NVARCHAR(256) = NULL,
+	@MailAddress NVARCHAR(256) = NULL,
+	@Password NVARCHAR(512) = NULL,
+	@IsManager BIT = NULL,
+	@IsAdmin BIT = NULL,
+	@VacationDayCount INT = NULL,
+	@Delete BIT = 0
+AS
+	IF (@Id IS NULL)
+	BEGIN
+		INSERT INTO [User](
+			DepartmentId,
+			FirstName,
+			LastName,
+			MailAddress,
+			[Password],
+			IsManager,
+			IsAdmin,
+			VacationDayCount
+		)
+		VALUES(
+			@DepartmentId,
+			@FirstName,
+			@LastName,
+			@MailAddress,
+			@Password,
+			@IsManager,
+			@IsAdmin,
+			@VacationDayCount
+		);
+	END
+RETURN 0
