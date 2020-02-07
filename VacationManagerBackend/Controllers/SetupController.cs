61adc1a7ab52260583d826e5ff51d4506adb14c2
Leon <leon.bruewer@tobit.software>
2020-02-07 09:08:34 +0100

    Add endpoint to setup configuration
    Add endpoint to get configuration

---
+++ VacationManagerBackend/Controllers/SetupController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoggerLibrary.Extension;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using VacationManagerBackend.Interfaces.Repositories;
+using VacationManagerBackend.Models;
+
+namespace VacationManagerBackend.Controllers
+{
+    public class SetupController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IConfigurationRepository _configurationRepository;
+
+        public SetupController(ILogger<SetupController> logger, IConfigurationRepository configurationRepository)
+        {
+            _configurationRepository = configurationRepository;
+        }
+
+        [HttpGet("config")]
+        public IActionResult Get()
+        {
+            var config = _configurationRepository.GetConfiguration();
+            if (config == null)
+            {
+                _logger.Info("No config found");
+                return NoContent();
+            }
+            return Ok(config);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] SetupData data)
+        {
+            if (data == null || !ModelState.IsValid)
+            {
+                _logger.Error("Invalid initialize data", new { data });
+                return BadRequest();
+            }
+            if (_configurationRepository.SetupConfig(data))
+            {
+                _logger.Info("VacationManager initialized", new { setupData = data });
+                return StatusCode(201);
+            }
+            return Conflict();
+        }
+    }
+}
---
+++ VacationManagerBackend/Models/SetupData.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VacationManagerBackend.Models
+{
+    public class SetupData
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string MailAddress { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string DepartmentName { get; set; }
+        public int? DefaultDayCount { get; set; }
+    }
+}
